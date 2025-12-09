b620287ba2ebd886681c2607d990bdc32cf2ee93
Hauerm <hauerm@edu.htl-villach.at>
2025-12-09 07:07:17 +0100

    Started working on a release memory function which will be called at the end of the programm to avoid memory leaks

--- main.c
+++ main.c
@@ -13,8 +13,9 @@ double get_time_ms();
 int main(int argc, char *argv[]) {
   // now since I wanted to test this with a benchmark I will do so
   printf("==========================================\n");
-  printf("Small Allocation Benchmark (64 bytes)\n");
+  printf("Small Allocation Benchmark (64 bytes + 32 bytes + 128 bytes)\n");
   printf("10.000 Allocations + 10.000 Frees\n");
+  printf("5.000 for 64 bytes, 3.000 for 32 bytes and 2.000 for 128 bytes\n");
   printf("==========================================\n\n");
 
   // 0 stand sfor the standart malloc
@@ -26,23 +27,43 @@ int main(int argc, char *argv[]) {
 }
 
 void benchmark_small_allocs(int which) {
-  const int NUM_ALLOCS = 10000;
-  const size_t SIZE = 64;
+  const int NUM_ALLOCS_64 = 5000;
+  const int NUM_ALLOCS_32 = 3000;
+  const int NUM_ALLOCS_128 = 2000;
+  const size_t SIZE_1 = 64;
+  const size_t SIZE_2 = 32;
+  const size_t SIZE_3 = 128;
 
-  void **pointers = malloc(NUM_ALLOCS * sizeof(void *));
+  void **pointers =
+      malloc((NUM_ALLOCS_64 + NUM_ALLOCS_32 + NUM_ALLOCS_128) * sizeof(void *));
 
   double start = get_time_ms();
-  for (int i = 0; i < NUM_ALLOCS; i++) {
+  for (int i = 0; i < NUM_ALLOCS_64; i++) {
     if (which) {
-      pointers[i] = memory_from_block(SIZE);
+      pointers[i] = memory_from_block(SIZE_1);
     } else {
-      pointers[i] = malloc(SIZE);
+      pointers[i] = malloc(SIZE_1);
+    }
+  }
+  for (int i = 0; i < NUM_ALLOCS_32; i++) {
+    if (which) {
+      pointers[i + NUM_ALLOCS_64 - 1] = memory_from_block(SIZE_2);
+    } else {
+      pointers[i + NUM_ALLOCS_64 - 1] = malloc(SIZE_2);
+    }
+  }
+  for (int i = 0; i < NUM_ALLOCS_128; i++) {
+    if (which) {
+      pointers[i + NUM_ALLOCS_64 + NUM_ALLOCS_32 - 1] =
+          memory_from_block(SIZE_3);
+    } else {
+      pointers[i + NUM_ALLOCS_64 + NUM_ALLOCS_32 - 1] = malloc(SIZE_3);
     }
   }
   double alloc_time = get_time_ms() - start;
 
   start = get_time_ms();
-  for (int i = 0; i < NUM_ALLOCS; i++) {
+  for (int i = 0; i < NUM_ALLOCS_64 + NUM_ALLOCS_32 + NUM_ALLOCS_128; i++) {
     if (which) {
       free_memory(pointers[i]);
     } else {
